4691941b758152e13f793586a1090738295c1113
rafajlpz <frilo_94@hotmail.com>
2021-11-02 09:40:39 +0100

    Iniciado ejercicio JS
    Creado js. Realizado ejercicios para alumnos

---
+++ java.js
@@ -0,0 +1,28 @@
+"use strict";
+// // EJERCICIO 1
+
+let myname = "rafa"
+console.log(myname)
+
+// // EJERCICIO 2
+
+myname = "rafael"
+console.log(myname)
+
+// //EJERCICIO 3//
+
+// myname = "rafael"
+// console.log(myname)  AL COMENTARLO DA ERROR
+
+//EJERCICIO 4
+
+let operador1 = 1;
+let operador2 = 2;
+let resultado = 0;
+
+resultado=operador1-operador2;
+console.log(resultado);
+
+operador1 = 4;
+console.log(operador1-operador2);
+console.log(resultado);
